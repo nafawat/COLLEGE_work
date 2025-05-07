7471cfa2fa2d1535c67b2c36d29290f4d10f9a11
NILAY JAIN <82969786+nafawat@users.noreply.github.com>
2025-05-08 01:56:12 +0530

    Add files via upload

---
+++ c program/even_odd.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a ; 
+    printf("enter your number : ");
+    scanf("%d",&a);
+    if(a%2 == 0)
+        printf("your number is even");
+    else{
+        printf("your number is odd");
+    }
+
+}
